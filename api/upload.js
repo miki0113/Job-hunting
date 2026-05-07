6fbfbde1ba8475abd08a48d33a7aa9da9692986a
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-07 22:52:48 +0900

    upload.js を更新

--- api/upload.js
+++ api/upload.js
@@ -6,13 +6,15 @@ export default async function handler(request) {
     const { searchParams } = new URL(request.url, `http://${request.headers.host}`);
     const filename = searchParams.get('filename');
 
-    if (!process.env.BLOB_READ_WRITE_TOKEN) {
+    // 1箇所目：ここを MY_ に修正
+    if (!process.env.MY_BLOB_READ_WRITE_TOKEN) {
       return new Response("トークンが見つかりません。Vercelの設定を確認してください。", { status: 500 });
     }
 
     const blob = await put(filename, file, {
       access: 'public',
-      token: process.env.BLOB_READ_WRITE_TOKEN,
+      // 2箇所目：ここも MY_ に修正
+      token: process.env.MY_BLOB_READ_WRITE_TOKEN,
     });
 
     return new Response(JSON.stringify(blob), {
