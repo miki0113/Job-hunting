b5a2ace0dea57c06df5181f5f7d5310c7092cfab
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-07 14:58:10 +0900

    list.js を更新

--- api/list.js
+++ api/list.js
@@ -1,7 +1,11 @@
 import { list } from '@vercel/blob';
 
 export default async function handler(request) {
-  const { blobs } = await list();
+  // カッコの中に合言葉（トークン）を書き足します
+  const { blobs } = await list({
+    token: process.env.BLOB_READ_WRITE_TOKEN
+  });
+  
   return new Response(JSON.stringify({ blobs }), {
     headers: { 'Content-Type': 'application/json' },
   });
