110ad71024b877b0facdef249e8e08881c357267
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-08 18:01:02 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// index.htmlを直接表示するだけのシンプルな命令
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// PDFのことは考えず、index.htmlを表示するだけの以前の形に戻します
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
   console.log('Server is running');
