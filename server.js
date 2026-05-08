6df9bb1b264a008397a97b4aaacfde54e9c54339
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-08 18:24:22 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const path = require('path');
-const multer = require('multer');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 保存先を一番単純な指定にします
-const upload = multer({ dest: 'uploads/' });
-
+// index.htmlを表示するだけの以前のシンプルな設定
 app.use(express.static(__dirname));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.send('アップロード完了！');
-});
-
-app.get('/api/list', (req, res) => {
-  res.json([]); // 一旦リストは空で返してエラーを防ぎます
-});
 
 app.listen(port, () => {
   console.log('Server is running');
