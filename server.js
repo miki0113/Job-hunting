84ae3b7d8b726b697fdd7c0d1b69ae33d4917c39
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-08 20:26:33 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
-app.listen(process.env.PORT || 3000);
+
+// 「404」を消すための窓口
+app.get('/api/list', (req, res) => {
+  res.json([]); // 今は中身を空にしてエラーを防ぎます
+});
+
+app.post('/upload', (req, res) => {
+  res.send('メンテナンス中');
+});
+
+app.listen(port, () => {
+  console.log('Running');
+});
