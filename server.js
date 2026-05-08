f47d7e9d5e10ff7eea94f149b60de08f637af2a8
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-08 20:17:18 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.static(__dirname));
-
-app.listen(port, () => {
-  console.log('Server is running');
-});
+app.listen(process.env.PORT || 3000);
